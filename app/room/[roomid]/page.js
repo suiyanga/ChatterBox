2998918d2770ca77d438043220b78417d899da33
suiyanga <augustkinya@gmail.com>
2024-07-09 13:40:09 +0300

    1st commit

---
+++ app/room/[roomid]/page.js
@@ -0,0 +1,46 @@
+'use client'
+
+import React from 'react';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+
+const Room = ({params}) => {
+  const roomID = params.roomid;
+    const myMeeting = async (element) => {
+      const appID = +process.env.NEXT_PUBLIC_APPID;
+      const serverSecret = process.env.NEXT_PUBLIC_SERVER_SECRET;
+      const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(
+        appID, 
+        serverSecret, 
+        roomID,  
+        Date.now().toString(),
+        "suiyanga"
+      );
+      const zp = ZegoUIKitPrebuilt.create(kitToken);
+      zp.joinRoom({
+        container: element,
+        sharedLinks: [
+          {
+            name: 'Personal link',
+            url:
+             window.location.protocol + '//' + 
+             window.location.host + window.location.pathname +
+              '?roomID=' +
+              roomID,
+          },
+        ],
+        scenario: {
+          mode: ZegoUIKitPrebuilt.OneONoneCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+        },
+    });
+  };
+
+
+  return (
+    <div 
+    ref={myMeeting}
+    className="w-[100w] h-[100vh]">
+    </div>
+  )
+};
+
+export default Room;
